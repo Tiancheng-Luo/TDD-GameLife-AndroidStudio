30e71d02193a5a6736a44ce9f62a915fdd35a902
Evan Liu <eidiot@gmail.com>
2014-09-06 11:50:11 +1200

    Test init

--- app/src/androidTest/java/me/eidiot/gameoflife/GameModelTest.java
+++ app/src/androidTest/java/me/eidiot/gameoflife/GameModelTest.java
@@ -4,8 +4,14 @@ import junit.framework.TestCase;
 
 public class GameModelTest extends TestCase {
 
+    GameModel instance;
     public void setUp() throws Exception {
         super.setUp();
+        instance = new GameModel(3, 3);
+    }
 
+    public void test_init() throws Exception {
+        assertEquals(3, instance.getRows());
+        assertEquals(3, instance.getColumns());
     }
 }
--- app/src/main/java/me/eidiot/gameoflife/GameModel.java
+++ app/src/main/java/me/eidiot/gameoflife/GameModel.java
@@ -1,4 +1,19 @@
 package me.eidiot.gameoflife;
 
 public class GameModel {
+    private int rows;
+    private int columns;
+
+    public GameModel(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
 }
